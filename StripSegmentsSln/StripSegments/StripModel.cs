716f5606a8fc4452b0d7647f5c887c1fedd37d64
Хасп Элд <EldHasp@Gmail.com>
2020-08-10 08:29:46 +0300

    Заливка локального Решения

---
+++ StripSegmentsSln/StripSegments/StripModel.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StripSegments
+{
+    /// <summary>Модель с исходными данными</summary>
+    public class StripModel
+    {
+        /// <summary>Асинхронное получение данных для первой коллекции.</summary>
+        /// <param name="range">Диапазон фильтрации.</param>
+        /// <returns>Коллекцию элементов прошедших фильтр.</returns>
+        public Task<IList<StripSegmentDto>> GetFirstSegmentsAsync(StripSegmentDto range)
+         => Task.Factory.StartNew(GetFirstSegments, range);
+
+        /// <summary>Перегрузка для использовании в Task.</summary>
+        /// <param name="range">Диапазон фильтрации.
+        /// Поступает в типе object, но должен содержать тип StripSegmentDto</param>
+        /// <returns>Коллекцию элементов прошедших фильтр.</returns>
+        private IList<StripSegmentDto> GetFirstSegments(object range)
+            => GetSecondSegments((StripSegmentDto) range);
+
+        /// <summary>Синхронное получение данных для первой коллекции.</summary>
+        /// <param name="range">Диапазон фильтрации.</param>
+        /// <returns>Коллекцию элементов прошедших фильтр.</returns>
+        public IList<StripSegmentDto> GetFirstSegments(StripSegmentDto range)
+        {
+            // Здесь обработка полученного диапазона и формирование отфильтрованной коллекции.
+
+            // Возврат для примера
+
+            return new StripSegmentDto[]
+            {
+                new StripSegmentDto(10,20),
+                new StripSegmentDto(30,50),
+                new StripSegmentDto(60,90)
+            };
+        }
+
+
+
+        /// <summary>Асинхронное получение данных для второй коллекции.</summary>
+        /// <param name="range">Диапазон фильтрации.</param>
+        /// <returns>Коллекцию элементов прошедших фильтр.</returns>
+        public Task< IList<StripSegmentDto>> GetSecondSegmentsAsync(StripSegmentDto range)
+            => Task.Factory.StartNew(GetSecondSegments, range);
+
+        /// <summary>Перегрузка для использовании в Task.</summary>
+        /// <param name="range">Диапазон фильтрации.
+        /// Поступает в типе object, но должен содержать тип StripSegmentDto</param>
+        /// <returns>Коллекцию элементов прошедших фильтр.</returns>
+        private IList<StripSegmentDto> GetSecondSegments(object range)
+            => GetSecondSegments((StripSegmentDto) range);
+
+        /// <summary>Синхронное получение данных для второй коллекции.</summary>
+        /// <param name="range">Диапазон фильтрации.</param>
+        /// <returns>Коллекцию элементов прошедших фильтр.</returns>
+        public IList<StripSegmentDto> GetSecondSegments(StripSegmentDto range)
+        {
+            // Здесь обработка полученного диапазона и формирование отфильтрованной коллекции.
+
+            // Возврат для примера
+
+            return new StripSegmentDto[]
+            {
+                new StripSegmentDto(10,20),
+                new StripSegmentDto(30,50),
+                new StripSegmentDto(60,90)
+            };
+        }
+    }
+}
---
+++ StripSegmentsSln/StripSegments/StripSegment.cs
@@ -0,0 +1,68 @@
+﻿using Common;
+using System;
+
+namespace StripSegments
+{
+    /// <summary>Класс Сегмента Полосы.</summary>
+    public class StripSegment : OnPropertyChangedClass, ICloneable<StripSegment>, IDto<StripSegmentDto>
+    {
+        #region Поля, хранящие значения одноимённых свойств
+        private double _begin;
+        private double _end;
+        private double _length;
+        private StripSegmentDto _dto;
+        #endregion
+
+        /// <summary>Начало Сегмента.</summary>
+        public double Begin { get => _begin; set => SetProperty(ref _begin, value); }
+        /// <summary>Конец Сегмента.</summary>
+        public double End { get => _end; set => SetProperty(ref _end, value); }
+        /// <summary>Длина Сегмента.</summary>
+        public double Length { get => _length; set => SetProperty(ref _length, value); }
+
+        protected override void PropertyNewValue<T>(ref T fieldProperty, T newValue, string propertyName)
+        {
+            base.PropertyNewValue(ref fieldProperty, newValue, propertyName);
+
+            // Пересчёт значений свойств при изменении одного их них.
+            switch (propertyName)
+            {
+                // Если изменяется Начало или Конец, то пересчитывается Длина.
+                case nameof(End):
+                case nameof(Begin):
+                    Length = Begin - End;
+                    break;
+
+                // Если изменяется длина, то пересчитывается Конец.
+                case nameof(Length):
+                    End = Begin + Length;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        #region Реализация интерфейса ICloneable<StripSegment>
+        public StripSegment Clone() => (StripSegment)((ICloneable)this).Clone();
+        object ICloneable.Clone() => MemberwiseClone();
+        #endregion
+
+        #region Реализация интерфейса IDto<StripSegmentDto>
+        public StripSegmentDto Dto { get => _dto; private set => SetProperty(ref _dto, value); }
+        public void SetDto(StripSegmentDto newDto) => CopyFrom(Dto = newDto);
+
+        #region Реализация интерфейса ICopy<StripSegmentDto>
+        public StripSegmentDto Copy() => new StripSegmentDto(Begin, End);
+
+        public void CopyFrom(StripSegmentDto dto) => (Begin, End) = (dto.Begin, dto.End);
+
+        public void CopyTo(StripSegmentDto obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+        #endregion
+    }
+}
